7d7a858d8706d2c9d1c1cbec09653376ecb456f6
Devon <=>
2019-06-18 08:50:38 -0700

    lecture codes

--- ASP/EFFunzies/Controllers/DashboardController.cs
+++ ASP/EFFunzies/Controllers/DashboardController.cs
@@ -24,5 +24,48 @@ namespace EFFunzies.Controllers
 
             return View(messages);
         }
+
+        [HttpGet("new")]
+        public IActionResult New()
+        {
+            ViewBag.UserId = SessionUser;
+            return View("New");
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(Message newMessage)
+        {
+            if(ModelState.IsValid)
+            {
+                // construct our message object
+                // newMessage.UserId = (int)SessionUser;
+
+                dbContext.Messages.Add(newMessage);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            ViewBag.UserId = SessionUser;
+            return View("New");
+        }
+
+        [HttpGet("vote/{isUpvote}/{messageId}")]
+        public IActionResult Vote(bool isUpvote, int messageId)
+        {
+            // make a vote!
+            Vote vote = new Vote();
+            // we need a user id!
+            vote.UserId = (int)SessionUser;
+            // we need a message id!
+            vote.MessageId = messageId;
+            // we need to know if down/upvote
+            vote.IsUpvote = isUpvote;
+
+            dbContext.Votes.Add(vote);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
     }
 }
--- ASP/EFFunzies/Models/Message.cs
+++ ASP/EFFunzies/Models/Message.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace EFFunzies.Models
 {
@@ -12,5 +13,15 @@ namespace EFFunzies.Models
         // Navigation Property
         public User Creator {get;set;}
         public List<Vote> VotesRecieved {get;set;}
+
+        // we can do cool stuff here!
+        public int? KarmaScore
+        {
+            get
+            {
+                if (this.VotesRecieved == null) return null;
+                return this.VotesRecieved.Count(v => v.IsUpvote) - this.VotesRecieved.Count(v => !v.IsUpvote);
+            }
+        }
     }
 }
